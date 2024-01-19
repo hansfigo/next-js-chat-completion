dc58f8c785b5fba909615436bb1a5ea5fdfabd47
HansFigo <hambasahaya0303@gmail.com>
2024-01-19 18:11:37 +0700

    chore(form.tsx): add missing semicolon to import statement for react in form.tsx style(form.tsx): update form layout to be responsive for different screen sizes chore(index.tsx): fix import statement for JetBrains_Mono font in index.tsx style(index.tsx): update layout for main container and form container to be responsive for different screen sizes

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,6 +1,6 @@
 import { completionAtom, isErrorAtom, isLoadingAtom } from "@/lib/atom";
 import { useAtom } from "jotai";
-import { ChangeEvent, FormEvent, useState } from "react"
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export const MyForm: React.FC = () => {
     // State untuk Loading, Respon (completion), input user dan Error
@@ -54,7 +54,7 @@ export const MyForm: React.FC = () => {
         }
     }
     return (
-        <form className="flex justify-centeritems-center gap-4" onSubmit={handleSubmit} >
+        <form className="flex justify-centeritems-center gap-4 flex-col md:flex-row" onSubmit={handleSubmit} >
             <input disabled={isLoading} onChange={handleChange} name='prompt' id='prompt' placeholder='Ask me anything !' type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
             <button disabled={isLoading} type="submit" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5   dark:border-gray-600 disabled:bg-gray-700">Submit</button>
         </form>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import { JetBrains_Mono } from 'next/font/google'
 import { MyForm } from '@/components/form'
 import { Skeleton } from '@/components/skeleton'
 import { getCompletion, getIsError, getIsLoading } from '@/lib/atom'
 import { useAtom } from 'jotai'
+import { JetBrains_Mono } from 'next/font/google'
 const jetbrain = JetBrains_Mono({ subsets: ['latin'] })
 
 const Home = () => {
@@ -14,7 +14,7 @@ const Home = () => {
     <main
       className={`flex min-h-screen flex-col items-center justify-between md:p-24 p-8 ${jetbrain.className}`}
     >
-      <div className='flex flex-col gap-4'>
+      <div className='flex xl:max-w-[800px] xl:min-w-[800px] px-8 xl:px-0 flex-col gap-4'>
         <h1 className='text-2xl md:text-4xl font-bold '>GPT Completion Demo 😈</h1>
         <MyForm />
 
